c4067ca3e4b66e2ea51664a9eb1304c53c09dfe1
dlaflotte <dlaflotte@gmail.com>
2023-03-19 16:56:49 -0400

    PID Tuned
    maybe?

--- Build2023/src/main/java/frc/robot/commands/TurnDegreesPID.java
+++ Build2023/src/main/java/frc/robot/commands/TurnDegreesPID.java
@@ -13,14 +13,14 @@ public class TurnDegreesPID extends PIDCommand {
     public TurnDegreesPID(double targetAngleDegrees, DriveTrain drive, Gyro gyro){
        
         super(
-            new edu.wpi.first.math.controller.PIDController(0.11, 0.01, 0.01),
+            new edu.wpi.first.math.controller.PIDController(0.2, 0.009, 0.025),
             gyro::getHeading,
             targetAngleDegrees,
             output->drive.turn(output),
             drive);
 
         getController().enableContinuousInput(-180, 180);
-        getController().setTolerance(1, 10);
+        getController().setTolerance(2, 100);
     }
     @Override
     public boolean isFinished(){
