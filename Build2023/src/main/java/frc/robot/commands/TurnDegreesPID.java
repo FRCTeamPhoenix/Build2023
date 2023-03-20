ff1baafcbb440d5292c49d1998618e785778490c
dlaflotte <dlaflotte@gmail.com>
2023-03-19 21:11:05 -0400

    Further PID Tweek
    Small changes to auto and PID

--- Build2023/src/main/java/frc/robot/commands/TurnDegreesPID.java
+++ Build2023/src/main/java/frc/robot/commands/TurnDegreesPID.java
@@ -13,14 +13,14 @@ public class TurnDegreesPID extends PIDCommand {
     public TurnDegreesPID(double targetAngleDegrees, DriveTrain drive, Gyro gyro){
        
         super(
-            new edu.wpi.first.math.controller.PIDController(0.2, 0.009, 0.025),
+            new edu.wpi.first.math.controller.PIDController(0.15, 0.009, 0.025),
             gyro::getHeading,
             targetAngleDegrees,
             output->drive.turn(output),
             drive);
 
         getController().enableContinuousInput(-180, 180);
-        getController().setTolerance(2, 100);
+        getController().setTolerance(4, 100);
     }
     @Override
     public boolean isFinished(){
